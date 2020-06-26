27586b2eda4be4d0cf9a2af1fc3221ac14bc3972
Dmytro <dmitry.tkk@gmail.com>
2020-06-26 19:05:30 +0300

    Add build folder to artifacts

--- deploy-manager.sh
+++ deploy-manager.sh
@@ -11,11 +11,12 @@ export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 docker pull skalenetwork/skale-manager:$MANAGER_TAG
 docker run \
-    -v $DIR/contracts_data:/usr/src/manager/data \
-    --mount type=volume,dst=/usr/src/manager/.openzeppelin,volume-driver=local,volume-opt=type=none,volume-opt=o=bind,volume-opt=device=$DIR/.openzeppelin \
+    -v $DIR/artifacts/contracts_data:/usr/src/manager/data \
+    -v $DIR/artifacts/build:/usr/src/manager/build \
+    --mount type=volume,dst=/usr/src/manager/.openzeppelin,volume-driver=local,volume-opt=type=none,volume-opt=o=bind,volume-opt=device=$DIR/artifacts/openzeppelin \
     -e ENDPOINT=$ENDPOINT \
     -e PRIVATE_KEY=$ETH_PRIVATE_KEY \
     skalenetwork/skale-manager:$MANAGER_TAG \
     npx truffle migrate --network unique 2>&1 | tee -a sm-container.log
 
-cp $DIR/contracts_data/unique.json $DIR/abi.json
+cp $DIR/artifacts/contracts_data/unique.json $DIR/artifacts/abi.json
