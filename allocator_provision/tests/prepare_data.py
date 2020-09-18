dce19385f740b5eb903a4c057928bf17eb614411
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2020-09-18 14:46:41 +0300

    SKALE-2999 Generate pk_file

--- allocator_provision/tests/prepare_data.py
+++ allocator_provision/tests/prepare_data.py
@@ -3,11 +3,12 @@
 from skale.utils.helper import init_default_logger
 
 from core import init_skale_allocator
-from tests.constants import TEST_PK_FILE, ENDPOINT
+from tests.constants import TEST_PK, TEST_PK_FILE, ENDPOINT
 
 
 def main():
     init_default_logger()
+    generate_pk_file()
     skale_allocator = init_skale_allocator(
         endpoint=ENDPOINT,
         pk_file=TEST_PK_FILE
@@ -15,6 +16,11 @@ def main():
     vesting_manager_role = skale_allocator.allocator.vesting_manager_role()
     skale_allocator.allocator.grant_role(vesting_manager_role, skale_allocator.wallet.address)
     # transfer_tokens_to_allocator()
+
+
+def generate_pk_file():
+    with open(TEST_PK_FILE, 'w+') as f:
+        f.write(TEST_PK)
 
 
 if __name__ == '__main__':
